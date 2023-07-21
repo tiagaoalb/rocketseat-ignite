badbebfa48c861345d1956e0b7ea8ae7201b5d78
Tiago Bovolin <tiagaoalb@gmail.com>
2023-07-20 21:52:29 -0300

    chore: update button style

--- 02-ignite-timer/src/components/Button.styles.ts
+++ 02-ignite-timer/src/components/Button.styles.ts
@@ -22,8 +22,12 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  background-color: ${props => props.theme.primary};
+  background-color: ${props => props.theme['green-500']};
+  color: ${props => props.theme.white};
 
   /* ${props => {
     return css`
